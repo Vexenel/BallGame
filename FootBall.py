2604ac3fa17d051d336216cadc04e944c23ceac9
Vexenel <107070246+Vexenel@users.noreply.github.com>
2022-06-10 17:29:33 +0300

    Add files via upload

--- FootBall.py
+++ FootBall.py
@@ -3,72 +3,95 @@ import pygame
 import random
 
 
-def main() -> None:
-    # Подготовка к работе
-    pygame.init()
+# Подготовка к работе
+pygame.init()
 
-    # Настройка фпс
-    FPS = 60
-    clock = pygame.time.Clock()
+# Настройка фпс
+FPS = 150
+clock = pygame.time.Clock()
 
-    # Создание переменных
-    screen_width = 674
-    screen_height = 1024
-    DISPLAYSURFACE = pygame.display.set_mode((674, 1024))
-    xpos = 50
-    ypos = 50
-    speedx = 5
-    speedy = 5
-    score = 0
+# Создание переменных
+screen_width = 674
+screen_height = 1024
+xpos = 338
+ypos = 513
+xpos1 = 420
+ypos2 = 940
+xpos2 = 250
+ypos3 = 10
+speedx = 5
+speedy = 5
+score1 = 0
+score2 = 0
+speed1 = 5
+speed2 = 5
 
-    # Создание цветов
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
+# Создание цветов
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-    # Загрузка и настройка картинок 
-    background = pygame.image.load("back.png")
-    image = pygame.image.load("bl.png")
-    image.set_colorkey((255, 255, 255))
-    image.set_alpha(200)
+# Загрузка и настройка картинок
+background = pygame.image.load("back.png")
+pl1 = pygame.image.load("vr.png")
+pl2 = pygame.image.load("vrl.png")
+icon = pygame.image.load("fb.png")
+pygame.display.set_icon(icon)
+ball = pygame.image.load("bl.png")
+ball.set_colorkey((255, 255, 255))
+ball.set_alpha(200)
 
-    # Создание экрана и его название
-    DISPLAYSURFACE = pygame.display.set_mode((674, 1024))
-    pygame.display.set_caption("FootBall Game")
+# Создание экрана и его название
+DISPLAYSURFACE = pygame.display.set_mode((674, 1024))
+pygame.display.set_caption("Ball Game")
 
-    # Добавление шрифтов
-    font = pygame.font.SysFont("Chilanka", 100)
-    font_small = pygame.font.SysFont("Chilanka", 100)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-                running = False
-        if xpos > screen_width-64:
-            speedx = (-5 * random.random())-1
-        if xpos < 0:
-            speedx = (5 * random.random())+1
-        if ypos > screen_height-64:
-            speedy = (-5 * random.random())-1
-        if ypos < 0:
-            speedy = (5 * random.random())+1
-        xpos += speedx
-        ypos += speedy
-        DISPLAYSURFACE.blit(background, (0, 0))
-        DISPLAYSURFACE.blit(image, (xpos, ypos))
-        scores = font_small.render(str(score), True, BLACK)
-        DISPLAYSURFACE.blit(scores, (10, 10))
-        DISPLAYSURFACE.blit(scores, (10, 930))
-        pygame.display.flip()
-        clock.tick(150)
+# Добавление шрифтов
+font = pygame.font.SysFont("Chilanka", 100)
+font_small = pygame.font.SysFont("Chilanka", 100)
+game_over = font.render("Game Over", True, BLACK)
 
 
-if __name__ == "__main__":
-    main()
-
-
-
-
-
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        speed1 -= 3
+    elif keys[pygame.K_RIGHT]:
+        speed1 += 3
+    if keys[pygame.K_a]:
+        speed2 -= 3
+    elif keys[pygame.K_d]:
+        speed2 += 3
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                xpos = event.pos[0] - 75
+                ypos = event.pos[1] - 75
+                print(event.pos)
+    if xpos > screen_width-64:
+        speedx = (-5 * random.random())-1
+    if xpos < 0:
+        speedx = (5 * random.random())+1
+    if ypos > screen_height-64:
+        speedy = (-5 * random.random())-1
+        score2 += 1
+    if ypos < 0:
+        speedy = (5 * random.random())+1
+        score1 += 1
+    xpos1 = speed1
+    xpos2 = speed2
+    xpos += speedx
+    ypos += speedy
+    DISPLAYSURFACE.blit(background, (0, 0))
+    DISPLAYSURFACE.blit(ball, (xpos, ypos))
+    DISPLAYSURFACE.blit(pl1, (xpos1, ypos2))
+    DISPLAYSURFACE.blit(pl2, (xpos2, ypos3))
+    scores1 = font_small.render(str(score1), True, BLACK)
+    scores2 = font_small.render(str(score2), True, BLACK)
+    DISPLAYSURFACE.blit(scores1, (10, 10))
+    DISPLAYSURFACE.blit(scores2, (10, 930))
+    pygame.display.flip()
+    clock.tick(FPS)
